9da69468a48b9b81b453c021fbf78c72740c5bf7
Adrian Hugentobler <adrian.hugentobler@outlook.com>
2024-05-27 20:20:41 +0200

    test asio read_until for reading header only

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ std::vector<char> vBuffer(20 * 1024);
 
 void grabSomeData(asio::ip::tcp::socket& socket)
 {
-    socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+    asio::async_read_until(socket, asio::dynamic_buffer(vBuffer), "\r\n\r\n",
         [&](std::error_code ec, std::size_t length)
         {
             if (!ec)
@@ -25,12 +25,28 @@ void grabSomeData(asio::ip::tcp::socket& socket)
                 for (int i = 0; i < length; i++)
                 {
                     std::cout << vBuffer[i];
-
-                    grabSomeData(socket);
                 }
             }
         }
     );
+
+
+    // socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+    //     [&](std::error_code ec, std::size_t length)
+    //     {
+    //         if (!ec)
+    //         {
+    //             std::cout << "\n\nRead " << length << " bytes\n\n";
+
+    //             for (int i = 0; i < length; i++)
+    //             {
+    //                 std::cout << vBuffer[i];
+
+    //                 grabSomeData(socket);
+    //             }
+    //         }
+    //     }
+    // );
 }
 
 int main(int argc, char const *argv[])
@@ -43,8 +59,8 @@ int main(int argc, char const *argv[])
 
     std::thread thrContext = std::thread([&]() { context.run(); });
 
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("77.109.136.75", ec), 80);
-    // asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.215.14", ec), 80);
+    // asio::ip::tcp::endpoint endpoint(asio::ip::make_address("77.109.136.75", ec), 80);
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.215.14", ec), 80);
     // asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
 
     asio::ip::tcp::socket socket(context);
@@ -65,16 +81,16 @@ int main(int argc, char const *argv[])
         grabSomeData(socket);
 
         std::string sRequest =
-            "GET /apis/site/v2/sports/football/nfl/scoreboard?dates=2023&seasontype=2&week=1 HTTP/1.1\r\n"
-            "Host: site.api.espn.com\r\n"
-            // "GET /index.html HTTP/1.1\r\n"
-            // "Host: example.com\r\n"
+            // "GET /apis/site/v2/sports/football/nfl/scoreboard?dates=2023&seasontype=2&week=1 HTTP/1.1\r\n"
+            // "Host: site.api.espn.com\r\n"
+            "GET /index.html HTTP/1.1\r\n"
+            "Host: example.com\r\n"
             "Connection: close\r\n\r\n";
 
         socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
     }
 
-    std::this_thread::sleep_for(2000ms);
+    std::this_thread::sleep_for(5000ms);
 
     context.stop();
     if (thrContext.joinable()) thrContext.join();
