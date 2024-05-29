5306b0a30da35433161f43fd6fccffb934e91a7d
Adrian Hugentobler <adrian.hugentobler@outlook.com>
2024-05-29 20:27:18 +0200

    reading body only. convert to json file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <fstream>
+#include <string>
 
 #include <chrono>
 using namespace std::chrono_literals;
@@ -11,42 +13,83 @@ using namespace std::chrono_literals;
 #include "libs/nlohmann/json.hpp"
 
 std::vector<char> vBuffer(20 * 1024);
+std::vector<std::string> vBuffer_Body(20 * 1024);
+
+// void grabSomeData(asio::ip::tcp::socket& socket)
+// {
+//     // asio::async_read_until(socket, asio::dynamic_buffer(vBuffer), "\r\n\r\n",
+//     //     [&](std::error_code ec, std::size_t length)
+//     //     {
+//     //         if (!ec)
+//     //         {
+//     //             std::cout << "\n\nRead " << length << " bytes\n\n";
+
+//     //             for (int i = 0; i < length; i++)
+//     //             {
+//     //                 std::cout << vBuffer[i];
+//     //             }
+//     //         }
+//     //     }
+//     // );
 
 
-void grabSomeData(asio::ip::tcp::socket& socket)
-{
-    asio::async_read_until(socket, asio::dynamic_buffer(vBuffer), "\r\n\r\n",
-        [&](std::error_code ec, std::size_t length)
+//     socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+//         [&](std::error_code ec, std::size_t length)
+//         {
+//             if (!ec)
+//             {
+//                 std::cout << "\n\nRead " << length << " bytes\n\n";
+
+//                 for (int i = 0; i < length; i++)
+//                 {
+//                     // std::cout << vBuffer[i];
+//                     grabSomeData(socket);
+//                 }
+//             }
+//         }
+//     );
+// }
+
+void grabSomeData(asio::ip::tcp::socket& socket);
+
+void handleRead(asio::ip::tcp::socket& socket, std::vector<char>& vBuffer, std::size_t length) {
+    std::string data(vBuffer.data(), length);
+    static bool headerProcessed = false;
+    static std::string headerBuffer;
+
+    if (!headerProcessed) {
+        headerBuffer += data;
+        std::size_t headerEnd = headerBuffer.find("\r\n\r\n");
+        if (headerEnd != std::string::npos) {
+            headerProcessed = true;
+            std::string body = headerBuffer.substr(headerEnd + 4);
+            // std::cout << "Header:\n" << headerBuffer.substr(0, headerEnd) << "\n";
+            // std::cout << "Bodyy:\n" << body << "\n";
+            vBuffer_Body.emplace_back(body);
+            // Handle the body data as needed, or start processing subsequent reads directly as body.
+        }
+    }
+    else {
+        // std::cout << "Body:\n" << data << "\n";
+        vBuffer_Body.emplace_back(data);
+        // Handle the body data as needed.
+    }
+
+    grabSomeData(socket);
+}
+
+void grabSomeData(asio::ip::tcp::socket& socket) {
+    auto vBuffer = std::make_shared<std::vector<char>>(1024);
+    socket.async_read_some(asio::buffer(vBuffer->data(), vBuffer->size()),
+        [vBuffer, &socket](std::error_code ec, std::size_t length)
         {
             if (!ec)
             {
                 std::cout << "\n\nRead " << length << " bytes\n\n";
-
-                for (int i = 0; i < length; i++)
-                {
-                    std::cout << vBuffer[i];
-                }
+                handleRead(socket, *vBuffer, length);
             }
         }
     );
-
-
-    // socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
-    //     [&](std::error_code ec, std::size_t length)
-    //     {
-    //         if (!ec)
-    //         {
-    //             std::cout << "\n\nRead " << length << " bytes\n\n";
-
-    //             for (int i = 0; i < length; i++)
-    //             {
-    //                 std::cout << vBuffer[i];
-
-    //                 grabSomeData(socket);
-    //             }
-    //         }
-    //     }
-    // );
 }
 
 int main(int argc, char const *argv[])
@@ -59,8 +102,8 @@ int main(int argc, char const *argv[])
 
     std::thread thrContext = std::thread([&]() { context.run(); });
 
-    // asio::ip::tcp::endpoint endpoint(asio::ip::make_address("77.109.136.75", ec), 80);
-    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.215.14", ec), 80);
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("54.71.61.211", ec), 80);
+    // asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.215.14", ec), 80);
     // asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
 
     asio::ip::tcp::socket socket(context);
@@ -81,16 +124,31 @@ int main(int argc, char const *argv[])
         grabSomeData(socket);
 
         std::string sRequest =
-            // "GET /apis/site/v2/sports/football/nfl/scoreboard?dates=2023&seasontype=2&week=1 HTTP/1.1\r\n"
-            // "Host: site.api.espn.com\r\n"
-            "GET /index.html HTTP/1.1\r\n"
-            "Host: example.com\r\n"
+            "GET /v2/sports/football/leagues/nfl/events HTTP/1.1\r\n"
+            "Host: sports.core.api.espn.com\r\n"
+            // "GET /index.html HTTP/1.1\r\n"
+            // "Host: example.com\r\n"
             "Connection: close\r\n\r\n";
 
         socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
     }
 
-    std::this_thread::sleep_for(5000ms);
+    std::this_thread::sleep_for(2000ms);
+
+    // std::string json_string(vBuffer_Body.begin(), vBuffer_Body.end());
+
+    std::ofstream myfile;
+    myfile.open("filename.json");
+    for (auto s : vBuffer_Body)
+    {
+        myfile << s;
+    }
+    myfile.close();
+
+    std::ifstream ifs("filename.json");
+    nlohmann::json json_stream = nlohmann::json::parse(ifs);
+
+    std::cout << json_stream.at("$meta");
 
     context.stop();
     if (thrContext.joinable()) thrContext.join();
